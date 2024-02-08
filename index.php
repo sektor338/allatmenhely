32f4b04c082a4ef4dd3a607e1050dd420a4f14aa
FDRobi <fdrobi007@gmail.com>
2024-02-08 12:43:28 +0100

    navbar

--- index.php
+++ index.php
@@ -54,31 +54,11 @@
         Bedi fogalmazás -> hogyan segíthetünk
     -->
 
-    <nav class="p-4 border-b-2 border-accent absolute top-0 left-0 w-full">
-      <div class="text-text container mx-auto flex justify-between">
-        <div class="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="images/logo.png" alt="Logó" class="w-20" />
-          <a href="https://www.facebook.com/fradistakazallatokert/">
-            <span class="text-2xl font-bold text-accent"
-              >Fradisták Az Állatokért</span
-            ></a
-          >
-        </div>
-        <div class="max-sm:absolute right-5 top-5">
-          <div class="w-20 h-2 bg-white"></div>
-          <div class=""></div>
-          <div class=""></div>
-        </div>
-        <!-- az legyen aláhúzva amelyik témát a felhasználó látja -->
-          <div class="items-center text-lg flex space-x-6">
-              <a href="index.php">Fooldal</a>
-              <a href="supporters.php">Támogatóink</a>
-              <a href="about.php">Rólunk</a>
-              <a href="donation.php">Adományozás</a>
-              <a href="volunteer.php">Jelentkezz önkéntesnek</a>
-          </div>
-      </div>
-    </nav>
+      <?php
+      
+        require_once("./includes/navbar.php");
+
+      ?>
 
     <!-- KÉPEK -->
 
--- volunteer.php
+++ volunteer.php
@@ -46,30 +46,11 @@
 
 <body class="bg-background min-h-screen pt-[114px]">
 
-<nav class="p-4 border-b-2 border-accent absolute top-0 left-0 w-full">
-    <div class="text-text container mx-auto flex justify-between">
-        <div class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="images/logo.png" alt="Logó" class="w-20" />
-            <a href="index_2.html">
-            <span class="text-2xl font-bold text-accent"
-            >Fradisták Az Állatokért</span
-            ></a
-            >
-        </div>
-        <div class="max-sm:absolute right-5 top-5">
-            <div class="w-20 h-2 bg-white"></div>
-            <div class=""></div>
-            <div class=""></div>
-        </div>
-        <!-- az legyen aláhúzva amelyik témát a felhasználó látja -->
-        <div class="items-center text-lg flex space-x-6">
-            <a href="supporters.php">Támogatóink</a>
-            <a href="about.php">Rólunk</a>
-            <a href="donation.php">Adományozás</a>
-            <a href="volunteer.php">Jelentkezz önkéntesnek</a>
-        </div>
-    </div>
-</nav>
+<?php
+      
+      require_once("./includes/navbar.php");
+
+?>
 
 
 <div class="container mx-auto m-6 p-4 w-full h-screen">

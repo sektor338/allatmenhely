c7718c3cdafab3bfe27489b33fc0ec0b95622d39
LakatosRonaldo <144096918+DobaSandor@users.noreply.github.com>
2024-02-23 12:40:31 +0100

    Admin login on steroids

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,16 +1,17 @@
 <?php
+/*
 $servername = "";
 $username = "";
 $password = "";
 $dbname = "";
-/*
+*/
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "adatbazis";*/
+$dbname = "s24_allatmenhely";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-$sql = "SELECT * FROM `onkentesek`";
+$sql = "SELECT * FROM `users`";
 
 $res = mysqli_query($conn, $sql);
 

b2a466fa903bf0c3129a4f17ec47791fe275e688
Kitti0403 <106381308+Kitti0403@users.noreply.github.com>
2024-03-04 10:31:57 +0100

    About, Index fixes

--- index.php
+++ index.php
@@ -33,6 +33,7 @@
           @apply flex-grow bg-secondary rounded-lg p-5 mb-4 shadow-xl;
         }
         .section {
+          @apply flex-grow flex flex-col p-5 bg-primary rounded-lg min-h-[500px];
         }
       }
       @layer base {
@@ -49,15 +50,6 @@
   </head>
 
   <body class="bg-background min-h-screen pt-[114px]">
-    <!--  
-        Rezponzív?
-        Telefonon meglehessen nézni normálisan
-        Email: fradistakazallatokert@gmail.com       
-        állatokat lopni
-        <hamis hírek3
-        Bedi fogalmazás -> hogyan segíthetünk
-    -->
-
       <?php
       
         require_once("./includes/navbar.php");
@@ -65,12 +57,13 @@
       ?>
 
     <!-- KÉPEK -->
+    <!-- adatbázisból lekérés -->
     
-      <div class="flex max-[1000px]:flex-col text-2xl  max-md:text-xl max-sm:text-l container mx-auto flex-row m-6 p-4 w-full h-auto gap-8">
+      <div class="container flex flex-row max-[1000px]:flex-col max-md:text-xl max-sm:text-l text-2xl mx-auto m-6 p-4 w-full h-auto gap-8">
 
-      <div class="flex gap-8  max-md:flex-col">
+      <div class="flex gap-8 max-md:flex-col">
 
-        <div class="p-5 flex flex-col basis-1/2 bg-primary rounded-lg min-h-[500px]">
+        <div class="basis-1/2 section">
           <h1 class="text-2xl border-b-2 border-secondary mb-8 text-white">
             Fogadj örökbe!
           </h1>
@@ -86,21 +79,21 @@
 
       <!-- FOGALMAZÁS -->
 
-        <div class="flex-grow flex flex-col p-5 basis-1/2 bg-primary rounded-lg min-h-[500px]">
+        <div class="basis-1/2 section">
           <h1 class="text-2xl border-b-2 border-secondary mb-8 text-white">
             Hogyan segíthettek
           </h1>
           <div class="flex flex-col flex-grow">
             <div class="cella">
-              <span><u class="text-2xl">Adományozz</u><br> Pénzadományokat fogadunk, amelyeket az állatok ellátására és a menhely fenntartására használunk fel.
+              <span><u class="text-2xl">Adományozz</u><br>Pénzadományokat fogadunk, amelyeket az állatok ellátására és a menhely fenntartására használunk fel.
               Az adományokat közvetlenül a weboldalunkon keresztül lehet megtenni.</span>
             </div>
-            <div class="cella ">
+            <div class="cella">
               <span><u class="text-2xl">Önkénteskedj</u><br> Mindig szükségünk van önkéntesekre, 
               akik segítenek az állatok gondozásában, takarításban, vagy akár az eseményeink szervezésében.</span>
             </div>
             <div class="cella">
-              <span><u class="text-2xl">Adoptálj egy állatot</u> <br> Sok szeretetre méltó állat várja, hogy örök otthonra találjon.
+              <span><u class="text-2xl">Adoptálj egy állatot</u><br> Sok szeretetre méltó állat várja, hogy örök otthonra találjon.
                 Tekintsd meg az örökbefogadható állatainkat a weboldalunkon.</span>
             </div>
             <div class="cella">
@@ -118,9 +111,10 @@
         </div>
       
       </div>
-      <!-- LE HÍREK -->
+      <!-- HÍREK -->
+      <!-- adatbázisból lekérés -->
 
-      <div class="flex-grow flex flex-col p-5 basis-1/4 bg-primary rounded-lg min-h-[500px]">
+      <div class="basis-1/4 section">
           <h1 class="text-2xl border-b-2 border-secondary mb-8 text-white">
             Hírek
           </h1>
